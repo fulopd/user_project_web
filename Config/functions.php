7217152c7c67d02b6657b732bb444f73e508a2c1
fulopd <fulopdaniel@gmail.com>
2020-01-30 06:53:45 +0100

    jogosultság ellenőrzés SQL lekérdezés szépítése

--- Config/functions.php
+++ Config/functions.php
@@ -12,78 +12,70 @@ function isLogged() {
     }
 }
 
-function isHaveRequiredPermission($con, $requiredPermission){
-        
+function isHaveRequiredPermission($con, $requiredPermission) {
+
     $userid = $_SESSION['userid'];
 
-    $sql = "SELECT position_id FROM user_data WHERE id = $userid";
-    $stmt = $con -> prepare($sql);    
-    $stmt -> execute();
-    $stmt -> store_result();    
-    $stmt -> bind_result($position_id);
-    $stmt -> fetch();    
-    
-    $sql = "SELECT position_name, permission_ids FROM position WHERE id = $position_id";
-    $stmt = $con -> prepare($sql);    
-    $stmt -> execute();
-    $stmt -> store_result();    
-    $stmt -> bind_result($position_name, $permission_ids);
-    $stmt -> fetch();
-    
+    $sql = "SELECT position.permission_ids FROM position, user_data WHERE user_data.id = $userid AND user_data.position_id = position.id";
+    $stmt = $con->prepare($sql);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($permission_ids);
+    $stmt->fetch();
+
     $permissions = explode(',', $permission_ids);
-        
-    if (in_array($requiredPermission, $permissions)) {
-        //echo "benne van: ".$requiredPermission;
+
+    if (in_array($requiredPermission, $permissions)) {        
         return true;
     }
     return false;
 }
 
 function printMenu($con) {
-    
+
     $menu = file_get_contents('Html/menu.html');
     if (isLogged()) {
         $menuitems = '';
         //Beosztás menüpont megjelenítéséhez 2 -es jogosultság szükséges
-        if (isHaveRequiredPermission($con, 2)){
+        if (isHaveRequiredPermission($con, 2)) {
             $menuitems .= '<li class="nav-item"> <a class="nav-link text-light" href="beosztas.php">Beosztás </a></li>';
         }
         //Saját adatok menüpont megjelenítéséhez 1 -es jogosultság szükséges
-        if (isHaveRequiredPermission($con, 1)){
+        if (isHaveRequiredPermission($con, 1)) {
             $menuitems .= '<li class="nav-item"> <a class="nav-link text-light" href="userinfo.php">Saját adatok</a></li>';
         }
         $menuitems .= '</ul></div>'
-                    . '<div class="navbar-collapse">'
-                    . '<ul class="navbar-nav ml-auto">'
-                    . '<span class="nav-link text-warning">'.$_SESSION['username'].'</span>'
-                    . '<li class="nav-item"> <a class="nav-link text-light" href="logout.php"> Kilép</a> </li>'
-                    . '</ul>'
-                    . '</div>';
-        
+                . '<div class="navbar-collapse">'
+                . '<ul class="navbar-nav ml-auto">'
+                . '<span class="nav-link text-warning">' . $_SESSION['username'] . '</span>'
+                . '<li class="nav-item"> <a class="nav-link text-light" href="logout.php"> Kilép</a> </li>'
+                . '</ul>'
+                . '</div>';
+
         $menu = str_replace('::ki_belepes', $menuitems, $menu);
     } else {
         $menu = str_replace('::ki_belepes', '<li class="nav-item">  <a class="nav-link text-light" href="login.php">Belép</a> </li>', $menu);
     }
-    
+
     echo $menu;
 }
 
-function getFirsDay($date){
+function getFirsDay($date) {
     $modDate = (new DateTime($date))
-      ->modify('first day of this month')
-      ->format('Y-m-d');
+            ->modify('first day of this month')
+            ->format('Y-m-d');
 
     return $modDate;
 }
 
-function getLastDay($date){
+function getLastDay($date) {
     $modDate = (new DateTime($date))
-      ->modify('last day of this month')
-      ->format('Y-m-d');
+            ->modify('last day of this month')
+            ->format('Y-m-d');
 
     return $modDate;
 }
 
-function increaseDateDay($date, $day){    
-    return date('Y-m-d', strtotime($date . ' +'.$day.' day'));    
+function increaseDateDay($date, $day) {
+    return date('Y-m-d', strtotime($date . ' +' . $day . ' day'));
 }
