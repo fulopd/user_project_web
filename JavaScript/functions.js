cc3506b6d4f0b5d6e307485e2a1ce17336ba8e7a
fulopd <fulopdaniel@gmail.com>
2020-03-17 10:51:24 +0100

    kereső mezők kinézet átalakítása

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     $(".filter-icon").click(function () {
 
-        $("#filter-box").toggle(150);
+        $("#filter-box").toggle(150);        
+        $(".filter-icon").toggleClass( "filter-icon-sec" );               
         /*console.log($("#filter-box").css('display'));
          if ($("#filter-box").css('display') === "none") {
          $("#filter-box").show(150);
